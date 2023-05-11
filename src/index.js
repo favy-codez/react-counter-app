df0c42f40f66dd1b36b225808962038320e93489
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-05-11 19:34:49 +0100

    finale

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// Assignment
+// Count App with two buttons, to incement and decrement
+// I also want to see the Count Value
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
